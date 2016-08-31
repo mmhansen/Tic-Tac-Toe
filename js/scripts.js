5c42ea034dee8462411183a92ad5bd78fd4174ca
Michael Hansen <hansen.mmb@gmail.com>
2016-08-30 20:22:33 -0400

    restyle and built most of game components

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,32 @@ $(document).ready(function(){
 /////////////////////
 // Event Listeners //
 /////////////////////
-$('#win').click(function(){
 
+// menu buttons 
+$('#win').hover(function(){
+	//$(this).toggleClass();
 })
-$('#loss').click(function(){
-
+$('#loss').hover(function(){
+	//$(this).toggleClass();
 })
 $('#restart').click(function(){
-	console.log("true");
-	 $('#modal1').openModal();
-
+	 app.restart();
 })
-$('#1').click(function(){
-
+// modal buttons
+$('#X').click(function(){
+	app.userChar = "X";
+	app.computerChar = "O";
 })
-$('#2').click(function(){
-
+$('#O').click(function(){
+	app.userChar = "O";
+	app.computerChar = "X";
 })
-$('#3').click(function(){
-
-})
-$('#4').click(function(){
-
-})
-$('#5').click(function(){
-
-})
-$('#6').click(function(){
-
-})
-$('#7').click(function(){
-
-})
-$('#8').click(function(){
-
-})
-$('#9').click(function(){
-
+// game board
+$('.tile').click(function(){
+	var clicked = $(this).attr('id');
+	if (app.checkSpaceOccupied(clicked)) {
+		app.makeMove(clicked);
+	}
 })
 
 /////////
@@ -47,26 +36,119 @@ $('#9').click(function(){
 /////////
 
 var app = {
-	chosenChar : false,
 	boardSpaces : ['#','#','#','#','#','#','#','#','#'],
-	winningPermutations : [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]],	
+	winningPermutations : [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]],
+	userMoves : [],
+	computerMoves : [],
+	turn : 1,
 };
 
-app.showModal = function(){
+app.checkSpaceOccupied = function(id){
+	var space = $('#'+id).text();
+	if (space) {
+		return false;
+	} else {
+		return true;
+	}
+}
 
-};
-app.hideModal = function(){
+app.restart = function(){
+	// empty board
+	$('.tile').text("");
+	// reset characters
+	app.userChar = null;
+	app.computerChar = null;
+	app.turn = 1;
+	// open modal
+	$('#modal1').openModal();
+}
+
+// triggered on click
+app.makeMove = function(id){
+	// user odd turn (X)
+	if (app.userChar == "X"){
+		if (app.turn%2 == 1){
+			$("#"+id).text(app.userChar);
+			(app.userMoves).push(app.convertId(id));
+			
+		// even turn (O)
+		} else {
+			app.computer();
+			(app.computerMoves).push(app.convertId(id));
+		}
+	} else {
+		// computer odd turn
+		if (app.turn%2 == 1){
+			app.computer();
+			(app.computerMoves).push(app.convertId(id));
+		// even turn (O)
+		} else {
+			$("#"+id).text(app.userChar);
+			(app.userMoves).push(app.convertId(id));
+			
+		}
+	}
+
+app.turn++
+
 
 }
+
+
+app.computer = function(){
+	function getRandom(){
+		return (Math.floor(Math.random()*9))
+	}
+	var randomIn9 = getRandom(); 
+	var options = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+	if (app.checkSpaceOccupied(options[randomIn9])) {
+		$("#"+options[randomIn9]).text(app.computerChar);
+	} else {
+		app.computer();
+	}
+
+
+}
+
+app.convertId = function(id){
+	var square;
+	
+	if (id == "one") {
+		return 1;
+	} else if (id == "two") {
+		return 2;
+	} else if (id == "three") {
+		return 3;
+	} else if (id == "four") {
+		return 4;
+	} else if (id == "five") {
+		return 5;
+	} else if (id == "six") {
+		return 6;
+	} else if (id == "seven") {
+		return 7;
+	} else if (id == "eight") {
+		return 8;
+	} else if (id == "nine") {
+		return 9;
+	}
+}
+
 app.checkWinCondition = function(){
 
-}
-app.checkSpaceOccupied = function(){
+	// push the number clicked into array, check that array if it contains all 3 numbers in any order, in any winning condition
 
 }
 
 
 
+
+////////////////
+// Initialize //
+////////////////
+
+app.restart();
 
 // end doc ready
 })
